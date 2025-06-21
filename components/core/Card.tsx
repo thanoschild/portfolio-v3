a6b5f63a36d27820933d1c83b86ae25abfc4fa45
Sumeet Haldar <sumeethaldar731@gmail.com>
2025-06-21 22:10:23 +0530

    update ui

--- components/core/Card.tsx
+++ components/core/Card.tsx
@@ -30,7 +30,7 @@ export default function Card(props: CardProps) {
   const isProject = props.type === "project";
 
   return (
-    <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+    <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
       <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-[var(--sub-alt-color)]/80 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
 
       {!isProject ? (
@@ -99,7 +99,7 @@ export default function Card(props: CardProps) {
 
       {/* ... */}
       {isProject && props.icon && (
-        <div className="mt-4 sm:mt-0 flex items-center justify-center sm:order-1 sm:col-span-2 sm:translate-y-1">
+        <div className="sm:mt-0 flex items-center justify-center sm:order-1 sm:col-span-2 sm:translate-y-1">
           {React.cloneElement(props.icon, {
             className:
               "h-16 w-16 text-theme-text transition-transform duration-200 ease-in-out group-hover:scale-110 group-hover:text-[var(--main-color)]",
